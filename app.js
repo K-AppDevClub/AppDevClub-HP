c8c7261935d49553979c6e6b17f5c2601caddcc4
Tomohiro Matsuo <gacktomo@gmail.com>
2018-04-08 20:32:35 +0900

    レイアウトベース完成

--- app.js
+++ app.js
@@ -1,73 +1,30 @@
 require('./app.scss')
+import ShuffleText from 'shuffle-text';
 
-var front = { player: null, next: null, prev: null };
-var right = { player: null, next: null, prev: null };
-var back =  { player: null, next: null, prev: null };
-var left =  { player: null, next: null, prev: null };
+const resizeCanvas = function(){
+  let container = document.getElementById('canvas-container');
+  let canvas = document.getElementById('myCanvas');
+  canvas.width = container.clientWidth;
+  canvas.height = container.clientHeight; 
+}
 
-front.next = right;
-front.prev = left;
-right.next = back;
-right.prev = front;
-back.next  = left;
-back.prev  = right;
-left.next  = front;
-left.prev  = back;
-
-var current = front;
-var page_num = 0;
+let page_num = 0;
 
 onload=function(){
-  var angle = 0;
-  var cube = document.getElementById("cube");
+  let angle = 0;
+  const cube = document.getElementById("cube");
+  const el = document.getElementById("front");
+  const text = new ShuffleText(el);
+  text.start();
+  resizeCanvas();
 
-  stage = new createjs.Stage("myCanvas");
-  particleSystem = new particlejs.ParticleSystem();
+  const stage = new createjs.Stage("myCanvas");
+  const particleSystem = new particlejs.ParticleSystem();
+  const buble = { "bgColor": "#00000", "width": 728, "height": 115, "emitFrequency": "100", "startX": 365, "startXVariance": 0, "startY": 218, "startYVariance": 0, "initialDirection": 0, "initialDirectionVariance": "360", "initialSpeed": 8.5, "initialSpeedVariance": 0.1, "friction": "0.063", "accelerationSpeed": 0, "accelerationDirection": "275.8", "startScale": 3, "startScaleVariance": 0, "finishScale": "2", "finishScaleVariance": "0", "lifeSpan": 29, "lifeSpanVariance": "27", "startAlpha": "1", "startAlphaVariance": "0", "finishAlpha": "0", "finishAlphaVariance": "0", "shapeIdList": [ "blur_circle" ], "startColor": { "hue": 37, "hueVariance": 360, "saturation": "91", "saturationVariance": 0, "luminance": "56", "luminanceVariance": "16" }, "blendMode": true, "alphaCurveType": "0", "VERSION": "0.1.3" };
+  const blue = { "bgColor": "#00000", "width": 728, "height": 415, "emitFrequency": 20, "startX": 365, "startXVariance": 462, "startY": 218, "startYVariance": 376, "initialDirection": 0, "initialDirectionVariance": "360", "initialSpeed": 8.2, "initialSpeedVariance": "3.7", "friction": 0.1165, "accelerationSpeed": 0, "accelerationDirection": "360", "startScale": 0.88, "startScaleVariance": "1", "finishScale": 0.18, "finishScaleVariance": 0.68, "lifeSpan": 60, "lifeSpanVariance": 80, "startAlpha": "1", "startAlphaVariance": "0", "finishAlpha": 0.51, "finishAlphaVariance": 0.5, "shapeIdList": [ "blur_circle", "circle" ], "startColor": { "hue": 195, "hueVariance": 21, "saturation": 70, "saturationVariance": "0", "luminance": 77, "luminanceVariance": 37 }, "blendMode": true, "alphaCurveType": "1", "VERSION": "0.1.3" } ;
+  const pink = { "bgColor": "#00000", "width": 728, "height": 415, "emitFrequency": "100", "startX": 365, "startXVariance": "594", "startY": 218, "startYVariance": "222", "initialDirection": "0", "initialDirectionVariance": "360", "initialSpeed": "2", "initialSpeedVariance": "3.7", "friction": "0.038", "accelerationSpeed": "0.13", "accelerationDirection": "273.3", "startScale": "0.76", "startScaleVariance": "1", "finishScale": "0", "finishScaleVariance": "0", "lifeSpan": "50", "lifeSpanVariance": "196", "startAlpha": "1", "startAlphaVariance": "0", "finishAlpha": "0.35", "finishAlphaVariance": 0.5, "shapeIdList": [ "blur_circle" ], "startColor": { "hue": "312", "hueVariance": "55", "saturation": "71", "saturationVariance": "78", "luminance": "83", "luminanceVariance": "16" }, "blendMode": true, "alphaCurveType": "1", "VERSION": "0.1.3" };
   stage.addChild(particleSystem.container);
-
-  particleSystem.importFromJson(
-    {
-      "bgColor": "#00000",
-      "width": 728,
-      "height": 115,
-      "emitFrequency": "100",
-      "startX": 365,
-      "startXVariance": 0,
-      "startY": 218,
-      "startYVariance": 0,
-      "initialDirection": 0,
-      "initialDirectionVariance": "360",
-      "initialSpeed": 8.5,
-      "initialSpeedVariance": 0.1,
-      "friction": "0.063",
-      "accelerationSpeed": 0,
-      "accelerationDirection": "275.8",
-      "startScale": 3,
-      "startScaleVariance": 0,
-      "finishScale": "2",
-      "finishScaleVariance": "0",
-      "lifeSpan": 29,
-      "lifeSpanVariance": "27",
-      "startAlpha": "1",
-      "startAlphaVariance": "0",
-      "finishAlpha": "0",
-      "finishAlphaVariance": "0",
-      "shapeIdList": [
-          "blur_circle"
-      ],
-      "startColor": {
-          "hue": 37,
-          "hueVariance": 360,
-          "saturation": "91",
-          "saturationVariance": 0,
-          "luminance": "56",
-          "luminanceVariance": "16"
-      },
-      "blendMode": true,
-      "alphaCurveType": "0",
-      "VERSION": "0.1.3"
-  });
-
+  particleSystem.importFromJson(blue);
   createjs.Ticker.framerate = 60;
   createjs.Ticker.timingMode = createjs.Ticker.RAF;
   createjs.Ticker.addEventListener("tick", handleTick);
@@ -77,20 +34,21 @@ onload=function(){
     stage.update();
   }
 
+
+  // Event listners
+  window.addEventListener('resize', function (event) {
+    resizeCanvas();
+  });
+
   document.getElementById("next").onclick = function() {
     page_num += 1;
     angle -= 90;
     cube.style.webkitTransform = "rotateY("+ angle +"deg)";
-    current = current.next;
   };
 
   document.getElementById("back").onclick = function() {
     page_num -= 1;
     angle += 90;
     cube.style.webkitTransform = "rotateY("+ angle +"deg)";
-    current = current.prev;
-  };
-
-  document.getElementById("panel").onclick = function() {
   };
 }
